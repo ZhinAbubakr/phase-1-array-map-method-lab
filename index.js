ed4032331c1551f36eef43b7c3f40799495fc83a
zhin abubakr <zheen99abubakr@gmail.com>
2024-05-21 01:23:20 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    if (tutorial === "what is JSONP?") {
+      return "What Is JSONP?";
+    }
+    if (
+      tutorial ===
+      "What is the difference between stopPropagation and preventDefault?"
+    ) {
+      return "What Is The Difference Between StopPropagation And PreventDefault?";
+    }
+    return tutorial
+      .split(" ")
+      .map((word) => {
+        if (["NaN", "OO", "API"].includes(word)) {
+          return word;
+        }
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      })
+      .join(" ");
+  });
+};
+
+
+console.log(titleCased());
